55a5fb746fd76cc9d735fefa5ef108ad34e901ee
Ludo <ludorouge7@gmail.com>
2026-04-23 14:16:02 +0200

    modif fichier postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -12,18 +12,21 @@ const {
 } = require('../controllers/postController');
 const auth = require('../middleware/auth');
 
+const { toggleLike, getLikesByPost } = require('../controllers/likeController');
+
 const router = express.Router();
 
 // Publiques
 router.get('/', getPosts);
 router.get('/:id', getPostById);
+router.get('/:postId/comments', getCommentsByPost);
+router.get('/:postId/likes', getLikesByPost);
 
 // Protégées
 router.post('/', auth, createPost);
 router.put('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
-
-router.get ('/:postId/comments', getCommentsByPost);
 router.post('/:postId/comments', auth, createComment);
+router.post('/:postId/like', auth, toggleLike);
 
 module.exports = router;
