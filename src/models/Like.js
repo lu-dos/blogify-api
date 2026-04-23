b0037e6bb4366f7889b77fdf7833f4b957891ec8
Ludo <ludorouge7@gmail.com>
2026-04-23 14:00:32 +0200

    créa fichier Like.js

---
+++ src/models/Like.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema(
+    {
+        user : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        post : { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    },
+    { timestamps: true }
+);
+
+likeSchema.index({ user: 1, post: 1 }, { unique: true }); // Assure qu'un utilisateur ne peut liker un post qu'une seule fois
+
+module.exports = mongoose.model('Like', likeSchema);
