ea88a7306cd78eefc88565f9972f1c4a60456882
Ludo <ludorouge7@gmail.com>
2026-04-23 13:11:54 +0200

    userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const {register , login} = require('../controllers/userController');
+
+const router = express.Router();
+
+// Routes d'authentification
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
